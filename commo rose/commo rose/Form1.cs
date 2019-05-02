8ea15642f3b7badeaadfa9c51c93a1dab0eed1ad
ricardo <3xqwzxa5bnbl@mail.ru>
2019-05-02 12:37:37 +0700

    Button launces cmd when hovered after action button release

--- commo rose/commo rose/Form1.cs
+++ commo rose/commo rose/Form1.cs
@@ -21,6 +21,7 @@ namespace commo_rose
         const int SW_SHOWNORMAL = 1, SW_HIDE = 0;
         private Keys action_button;
         private KeyHandler ghk;
+        bool button_selected = false;
         public Form1()
         {
             InitializeComponent();
@@ -67,6 +68,10 @@ namespace commo_rose
             if(e.KeyCode == action_button)
             {
                 ShowWindow(this.Handle, SW_HIDE);
+                if(button_selected)
+                {
+                    button_action();
+                }
             }
             e.Handled = true;
         }
@@ -79,6 +84,7 @@ namespace commo_rose
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
+            button_selected = true;
             button1.BackColor = Color.Black;
             button1.ForeColor = Color.White;
             button1.FlatAppearance.MouseDownBackColor = button1.BackColor;
@@ -87,14 +93,24 @@ namespace commo_rose
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_MouseLeave(object sender, EventArgs e)
         {
+            button_selected = false;
             button1.BackColor = Color.White;
             button1.ForeColor = Color.Black;
             button1.FlatAppearance.BorderColor = button1.BackColor;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+
+        private void button_action()
+        {
+            System.Diagnostics.Process.Start("cmd");
         }
     }
 }
