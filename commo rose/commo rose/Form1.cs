b9785480c38719aa29df12cdd28b78df07395c2c
ricardo <3xqwzxa5bnbl@mail.ru>
2019-06-02 16:14:37 +0700

    Fixed: buttons not drawn instantly on first form show up

--- commo rose/commo rose/Form1.cs
+++ commo rose/commo rose/Form1.cs
@@ -74,6 +74,16 @@ namespace commo_rose
             settings = new Settings(this);
         }
 
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
